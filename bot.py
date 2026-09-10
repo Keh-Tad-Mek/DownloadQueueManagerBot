1ccf1bf698257b2374c4722ef803be587ef63e98
Kehali Tadesse <kehalitadesse707@gmail.com>
2026-09-10 22:06:54 +0300

    Initial push, basic welcome message to test the bot starts

--- bot.py
+++ bot.py
@@ -13,10 +13,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 def main():
+    # tells the server to listen to the bot that has this token
     app = Application.builder().token(TOKEN).build()
 
+    # all the commands that the bot can handle are added here
+    
     app.add_handler(CommandHandler("start", start))
 
+    # tells the bot to start polling for updates from Telegram
     app.run_polling()
 
 if __name__ == "__main__":
