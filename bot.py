2a6b9e09dcf2cc1332cc021dc7c6d84baa888d01
Kehali Tadesse <kehalitadesse707@gmail.com>
2026-09-10 19:57:19 +0300

    Initial push, basic welcome message to test the bot starts

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Welcome"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
